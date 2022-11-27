8f797d9fd31eaed48c508c1d76e0efe17fdab337
Nimus228 <peil20.12.2002@gmail.com>
2022-11-27 18:03:42 +0700

    add fast pow

--- oppo/oppo/oppo.cpp
+++ oppo/oppo/oppo.cpp
@@ -2,13 +2,25 @@
 
 using namespace std;
 double my_pow(double a, unsigned int b) {
-    long double x = a;
+    double x = a;
     for (int i = 0; i < b; i++) {
         x = x * a;
     }
     return x;
 }
+long int pow(long double x, unsigned int n)
+{
+    if (n == 0)
+        return 1;
+    else if (n == 1)
+        return x;
+    else if (n % 2 == 0)
+        return pow(x * x, n / 2);
+    else
+        return pow(x * x, n / 2) * x;
+}
 int main()
 {
+    cout << pow(3, 8);
     cout << my_pow(2, 8);
 }
