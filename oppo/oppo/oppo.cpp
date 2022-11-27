2942fda290d5529ea6b4c1c716e7e5f52f1f668e
Nimus228 <peil20.12.2002@gmail.com>
2022-11-27 17:37:55 +0700

    add my_pow

---
+++ oppo/oppo/oppo.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+using namespace std;
+double my_pow(double a, unsigned int b) {
+    long double x = a;
+    for (int i = 0; i < b; i++) {
+        x = x * a;
+    }
+    return x;
+}
+int main()
+{
+    cout << my_pow(2, 8);
+}
